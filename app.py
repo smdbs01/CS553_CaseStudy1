044c213313bb74e0cae1052c8b1af8501899b271
smdbs <1289707349@qq.com>
2024-09-05 19:17:43 -0400

    fix: change app.py to a gradio usage example
    Edited app.py and pinned the version of gradio to 4.42.0 and fastapi to 0.112 to avoid a breaking change of fastapi at 0.113.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-def main():
-    print('Hello, World!')
-    
-    
+import gradio as gr
+
+# Sample Gradio usage
+def greet(name, is_morning, temperature):
+    salutation = "Good morning" if is_morning else "Good evening"
+    greeting = f"{salutation} {name}. It is {temperature} degrees today"
+    celsius = (temperature - 32) * 5 / 9
+    return greeting, round(celsius, 2)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "checkbox", gr.Slider(0, 100)],
+    outputs=["text", "number"],
+)
 if __name__ == "__main__":
-    main()
+    demo.launch()
