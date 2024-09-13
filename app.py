5759d95a6e82e1656860409b4bd129352a6e643e
smdbs <1289707349@qq.com>
2024-09-13 14:02:25 -0400

    feat: change model to sd 2.1 base

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+from pathlib import Path
 from PIL import Image
 import gradio as gr
-from huggingface_hub import InferenceClient
 import numpy as np
 from io import BytesIO
 import os
@@ -8,72 +8,58 @@ from typing import Union
 from transformers import AutoImageProcessor, ResNetForImageClassification
 import torch
 import torch.nn.functional as F
+from huggingface_hub import InferenceClient
 
 # Make sure to set the environment variable HF_TOKEN to your Hugging Face token for using InferenceClient
 HF_TOKEN = os.environ.get("HF_TOKEN", "YOUR_HF_TOKEN")
-client = InferenceClient("microsoft/resnet-50", token=HF_TOKEN)
+model_name = "stabilityai/stable-diffusion-2-1-base"
+client = InferenceClient(model_name, token=HF_TOKEN)
 
 processor = None
 model = None
 
+
 def load_model():
     global processor, model
     print("Loading model...")
-    processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
-    model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+    # processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+    # model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
     print("Model loaded.")
 
-def resnet50(image: Union[np.ndarray, Image.Image, str, None], is_local: bool = False) -> dict[str, float]:
-    if image is None:
-        return {}
-    
+
+def sd_2_1_base(
+    prompt: str, is_local: bool
+) -> Union[np.ndarray, Image.Image, str, Path, None]:
     if is_local:
         if processor is None or model is None:
             load_model()
-        inputs = processor(image, return_tensors="pt")
-
-        with torch.no_grad():
-            logits = model(**inputs).logits
-
-        # Apply softmax to convert logits to probabilities
-        probabilities = F.softmax(logits, dim=-1)[0]
-
-        # Get the top 5 predicted classes with probabilities
-        top_probs, top_labels = torch.topk(probabilities, 5)
-
-        # Map the label IDs to human-readable class names
-        top_predictions = {model.config.id2label[top_labels[i].item()]: top_probs[i].item() for i in range(5)}
-
-        return top_predictions
+        pass
     else:
-        image_in = image
-        # Convert the image to bytes if it is a PIL image or numpy array
-        if isinstance(image, np.ndarray):
-            image = Image.fromarray(image)
-        if isinstance(image, Image.Image):
-            image_bytes = BytesIO()
-            image.save(image_bytes, format="PNG")
-            image_in = image_bytes.getvalue()
-        
-        # Get the model's prediction
-        prediction = client.image_classification(image_in)
-        # Return the top 5 results
-        return {pred.label: pred.score for pred in prediction}
+        output = client.text_to_image(prompt)
+        return output
 
 
 with gr.Blocks() as ui:
     with gr.Column():
-        gr.HTML("<h1 style='text-align: center;font-size: 48px;margin-bottom: 24px;'>ResNet-50 Image Classification</h1>")
+        gr.HTML(
+            "<h1 style='text-align: center;font-size: 48px;margin-bottom: 24px;'>😎 Cool Image Generator 😎</h1>"
+        )
         with gr.Row():
             with gr.Column():
-                image = gr.Image(label="Upload or drag and drop an image here")
-                submit = gr.Button("Submit", variant="primary")
+                prompt = gr.Textbox(
+                    "Enter a prompt to generate an image", label="Prompt"
+                )
+                submit = gr.Button("Generate Image", variant="primary")
             with gr.Column():
-                is_local = gr.Checkbox(label="Check this box to use a local model 🖥️", )
-                output = gr.Label(num_top_classes=5, label="Results will appear here", )
-        gr.HTML("<p style='text-align: center;'>This app uses the <a href='https://huggingface.co/microsoft/resnet-50'>microsoft/resnet-50</a> model.</p>")
-        
-    submit.click(resnet50, inputs=[image, is_local], outputs=output)
-    
+                is_local = gr.Checkbox(
+                    label="Check this box to use a local model 🖥️",
+                )
+                output = gr.Image(label="Generated Image")
+        gr.HTML(
+            f"<p style='text-align: center;'>This app uses the <a href='https://huggingface.co/{model_name}'>{model_name}</a> model.</p>"
+        )
+
+    submit.click(sd_2_1_base, inputs=[prompt, is_local], outputs=output)
+
 if __name__ == "__main__":
     ui.launch(share=True)
