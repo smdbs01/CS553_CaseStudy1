757c20bc099139e8da10c664c2db2065b184364f
smdbs <1289707349@qq.com>
2024-09-13 16:19:20 -0400

    Feat: add basic local model usage

--- app.py
+++ app.py
@@ -2,28 +2,38 @@ from pathlib import Path
 from PIL import Image
 import gradio as gr
 import numpy as np
-from io import BytesIO
 import os
 from typing import Union
-from transformers import AutoImageProcessor, ResNetForImageClassification
 import torch
 import torch.nn.functional as F
 from huggingface_hub import InferenceClient
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
 # Make sure to set the environment variable HF_TOKEN to your Hugging Face token for using InferenceClient
 HF_TOKEN = os.environ.get("HF_TOKEN", "YOUR_HF_TOKEN")
 model_name = "stabilityai/stable-diffusion-2-1-base"
 client = InferenceClient(model_name, token=HF_TOKEN)
 
-processor = None
-model = None
+scheduler = None
+pipe = None
+config = {
+    "negative_prompt": "",
+    "width": 256,
+    "height": 256,
+    "num_inference_steps": 20,
+}
 
 
 def load_model():
-    global processor, model
+    global scheduler, pipe
     print("Loading model...")
-    # processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
-    # model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+    scheduler = EulerDiscreteScheduler.from_pretrained(
+        model_name, subfolder="scheduler"
+    )
+    pipe = StableDiffusionPipeline.from_pretrained(model_name, scheduler=scheduler)
+    pipe.enable_attention_slicing()
+    if torch.cuda.is_available():
+        pipe = pipe.to("cuda")
     print("Model loaded.")
 
 
@@ -31,9 +41,14 @@ def sd_2_1_base(
     prompt: str, is_local: bool
 ) -> Union[np.ndarray, Image.Image, str, Path, None]:
     if is_local:
-        if processor is None or model is None:
+        if scheduler is None or pipe is None:
             load_model()
-        pass
+
+        image = pipe(
+            prompt,
+        ).images[0]
+        return image
+
     else:
         output = client.text_to_image(prompt)
         return output
@@ -47,7 +62,8 @@ with gr.Blocks() as ui:
         with gr.Row():
             with gr.Column():
                 prompt = gr.Textbox(
-                    "Enter a prompt to generate an image", label="Prompt"
+                    placeholder="Enter a prompt to generate an image",
+                    label="Prompt",
                 )
                 submit = gr.Button("Generate Image", variant="primary")
             with gr.Column():
